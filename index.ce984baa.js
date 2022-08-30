6f383a113d5ce139e01b1decb6d6d028a4a2cecc
NilKad <89694454+NilKad@users.noreply.github.com>
2022-08-30 12:46:20 +0000

    Deploying to gh-pages from @ NilKad/project-42@21cec96d23bb771aefacae832e18a51ce8e9af92 🚀

---
+++ index.ce984baa.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.forEach((function(o){return o.addEventListener("click",e)})),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.ce984baa.js.map
