71611ac8bff13864e7925eb0b8ba52c7bebe7c90
NilKad <89694454+NilKad@users.noreply.github.com>
2022-09-01 10:32:22 +0000

    Deploying to gh-pages from @ NilKad/project-42@2ea3b81b3e13fc62be08ab27eeed852781b72d76 🚀

--- index.45332353.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:3e3,stopOnLastSlide:!1,disableOnInteraction:!1}});
-//# sourceMappingURL=index.45332353.js.map
